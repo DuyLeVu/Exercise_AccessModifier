6b0c07f10fd87f1519e13ef05eac66d712213306
DuyLeVu <levuduy132@gmail.com>
2021-10-13 17:16:51 +0700

    Add exercise_accessmodifier

---
+++ exercise_accessmodifier/src/com/company/TestCircle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class TestCircle {
+
+        public static void main(String[] args) {
+            Circle circle = new Circle();
+            System.out.println(circle.getRadius());
+            System.out.println(circle.getArea());
+        }
+    }
+
